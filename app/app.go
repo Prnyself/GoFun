b5ead698ed911ae6122a1b424503810904411e74
zhouchengbin <zhouchengbin@caicloud.io>
2018-05-27 11:58:07 +0800

    feat(db):add migtate tool and query the database

--- app/app.go
+++ app/app.go
@@ -2,21 +2,30 @@ package app
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/go-xorm/xorm"
 	"github.com/xiaoheigou/GoFun/config"
+	"github.com/xiaoheigou/GoFun/db"
 )
 
 type Server struct {
 	port string
+	DB   *xorm.Engine
 }
 
 func NewServer() Server {
 	return Server{}
 }
 func (s *Server) Init() {
+	var err error
 	fmt.Println("Initialzing server.....")
 	s.port = fmt.Sprintf(":%d", config.HttpPort)
+	s.DB, err = db.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 func (s *Server) Start() {
 	fmt.Println("Starting the server listen on ", s.port)
--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/xiaoheigou/GoFun/app"
 )
 
 func main() {
 	s := app.NewServer()
 	s.Init()
+
+	result, err := s.DB.Query("select * from users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("table:%v", result)
+
 	s.Start()
 }
